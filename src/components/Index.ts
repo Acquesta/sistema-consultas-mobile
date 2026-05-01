5fa3cdf1822abcc306e33d5e70de73c800744129
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-30 22:30:34 -0300

    aula-6-navegacao-context-api

--- src/components/Index.ts
+++ src/components/Index.ts
@@ -1,5 +1,7 @@
-export { default as ConsultaCard } from "./ConsultaCard";
+/**
+ * Exportação centralizada dos componentes
+ */
 
+export { default as ConsultaCard } from "./ConsultaCard";
 export { default as Loading } from "./Loading";
 export { default as EmptyState } from "./EmptyState";
-export { default as Header } from "./Header";
